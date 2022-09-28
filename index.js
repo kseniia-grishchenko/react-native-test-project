05b01e6ea3c3531b62e3095828c925f7e07e602e
kseniia <kseniia@smartiot.cloud>
2022-09-28 22:56:37 +0300

    just test inserting own component

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import React from 'react';
+import {AppRegistry, Text} from 'react-native';
 import {name as appName} from './app.json';
+
+const App = () => {
+  return <Text>Hi There!</Text>;
+};
 
 AppRegistry.registerComponent(appName, () => App);
